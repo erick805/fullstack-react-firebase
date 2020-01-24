3a9570bdfb17aa6a566599fe671c2bf8f4c1a78e
Ricky Lau <rickylaufitness@gmail.com>
2020-01-24 17:29:36 -0500

    edit: map choices in Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Question({ question }) {
   return (
-    <div>
+    <>
       <h2>{question.question}</h2>
-      <div className="choice-container">
-        <p className="choice-prefix">1</p>
-        <p className="choice-text">{question.answerChoices[0]}</p>
-      </div>
-    </div>
+      {question.answerChoices.map((choice, idx) => (
+        <div className="choice-container">
+          <p className="choice-prefix">{idx + 1}</p>
+          <p className="choice-text">{choice}</p>
+        </div>
+      ))}
+    </>
   );
 }
