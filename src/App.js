a9e6f86955f8e422b5daecfb567b08a6d855d326
Ricky Lau <rickylaufitness@gmail.com>
2020-01-24 15:34:43 -0500

    create-edit: Home component into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
-import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Fragment className="App">
-      <h1>Open Trivia Quiz!</h1>
+    <Fragment>
+      <Home />
     </Fragment>
   );
 }
