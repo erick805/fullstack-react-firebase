e324fe1fef44d06e207fcf814f1cdfbf71c0c5bb
Ricky Lau <rickylaufitness@gmail.com>
2020-01-24 16:04:39 -0500

    edit-add: parent div in Game, HighScores, Home to empty, wrap App in div container

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import HighScores from "./components/HighScores";
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/game" component={Game} />
-      <Route path="/highScores" component={HighScores} />
+      <div className="container">
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+      </div>
     </Router>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Game() {
   return (
-    <div>
+    <>
       <h1>Game</h1>
-    </div>
+    </>
   );
 }
