b1d99cb48c4d23e0cd44f974934440aab5149886
Ricky Lau <rickylaufitness@gmail.com>
2020-01-24 20:09:52 -0500

    edit: fix encoding in Question h2 and choice-text

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,11 +3,14 @@ import React from "react";
 export default function Question({ question }) {
   return (
     <>
-      <h2>{question.question}</h2>
+      <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
       {question.answerChoices.map((choice, idx) => (
         <div key={idx} className="choice-container">
           <p className="choice-prefix">{idx + 1}</p>
-          <p className="choice-text">{choice}</p>
+          <p
+            className="choice-text"
+            dangerouslySetInnerHTML={{ __html: choice }}
+          ></p>
         </div>
       ))}
     </>
