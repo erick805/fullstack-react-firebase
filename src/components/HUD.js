fbd32ad1972ebce7fef5d193054a61a3750f21d7
Ricky Lau <rickylaufitness@gmail.com>
2020-01-25 11:22:52 -0500

    create: ProgressBar component in components dir

--- src/components/HUD.js
+++ src/components/HUD.js
@@ -4,10 +4,10 @@ export default function HUD({ score, questionNumber }) {
   return (
     <div id="hud">
       <div className="hud-item">
-        <p className="hub-prefix">Question {questionNumber}/10</p>
+        <p className="hud-prefix">Question {questionNumber}/10</p>
       </div>
       <div className="hud-item">
-        <p className="hub-prefix">Score</p>
+        <p className="hud-prefix">Score</p>
         <h1 className="hud-main-text">{score}</h1>
       </div>
     </div>
